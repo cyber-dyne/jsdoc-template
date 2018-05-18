070872aabdb500c6a00b9d7cd1466f84ef0fd270
Daniele Orlando <dnl.rlnd@gmail.com>
2018-05-18 09:03:52 +0200

    Navigation menu lists namespaces, modules and interfaces.

--- publish.js
+++ publish.js
@@ -352,15 +352,14 @@ function buildNav(members) {
   var seen = {};
   // var seenTutorials = {};
 
-  nav += buildMemberNav(members.classes, 'Classes', seen, linkto);
+  nav += buildMemberNav(members.namespaces, 'Namespaces', seen, linkto);
   nav += buildMemberNav(members.modules, 'Modules', {}, linkto);
-  // TODO: as needed, comment back in later
+  nav += buildMemberNav(members.classes, 'Classes', seen, linkto);
+  nav += buildMemberNav(members.interfaces, 'Interfaces', seen, linkto);
+  nav += buildMemberNav(members.mixins, 'Mixins', seen, linkto);
+  nav += buildMemberNav(members.events, 'Events', seen, linkto);
   // nav += buildMemberNav(members.externals, 'Externals', seen, linktoExternal);
-  // nav += buildMemberNav(members.events, 'Events', seen, linkto);
-  // nav += buildMemberNav(members.namespaces, 'Namespaces', seen, linkto);
-  // nav += buildMemberNav(members.mixins, 'Mixins', seen, linkto);
   // nav += buildMemberNav(members.tutorials, 'Tutorials', seenTutorials, linktoTutorial);
-  // nav += buildMemberNav(members.interfaces, 'Interfaces', seen, linkto);
 
   if (members.globals.length) {
     members.globals.forEach(function (g) {
